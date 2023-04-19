7cdc0963845fcbd18da63d00231b2846e567f695
Jeane-Cristina <117765404+Jeane-Cristina@users.noreply.github.com>
2023-04-19 18:32:07 -0300

    Add files via upload

---
+++ componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/R.jpg" alt="O banner principal da página do Organo"/>
+            <h1 className="texto">INSCREVA-SE!</h1>
+        </header>
+    )
+}
